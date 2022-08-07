4b9828afa363bacda1f59a82dad07b03d65bf1c0
RashadAlison <rashadaliosn213@gmail.com>
2022-08-07 12:50:21 +0800

    Updates

---
+++ precache-manifest.0960de5ffdc6276b8a4a0b67bf29f805.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aef6741c914e8ad6b30cd1b90d23f38",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "0c3805cc424d58943721",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d546830e1bb22b6298f1",
+    "url": "/react-frontend-dev-portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "0c3805cc424d58943721",
+    "url": "/react-frontend-dev-portfolio/static/js/2.cd1fa459.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.cd1fa459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d546830e1bb22b6298f1",
+    "url": "/react-frontend-dev-portfolio/static/js/main.0eb02281.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
